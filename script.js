a0cc404bb1041fe33cc2d02f7062e7d650020049
aabsalonsen <alberteabsalonsen@gmail.com>
2025-04-02 14:59:57 +0200

    slideshow, js, swipe funktion

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 //HEADER//
 //SECTION 2 - QUIZ//
 //SECTION 4 - SLIDESHOW
-//Manuelt slideshow 
+//Manuelt slideshow - swipe funktion
 let manualSlideIndex = 1;
+let startX = 0; 
+let endX = 0;
+let minSwipeDistance = 50;
 showSlidesManual (manualSlideIndex);
+
+document.querySelector ('.section-4').addEventListener('touchstart', function (e) {
+    startX = e.touches[0].clientX;
+});
+
+document.querySelector('.section-4').addEventListener('touchend', function (e) {
+    endX = e.changedTouches[0].clientX;
+    handleSwipe();
+});
+
+function handleSwipe() {
+    if (startX - endX > minSwipeDistance) {
+        //swipe til venstre - næste
+        manualSlideIndex++;
+        if (manualSlideIndex > document.getElementsByClassName('slide-box').length) {
+            manualSlideIndex = 1; //hvis sidste slide, gå tilbage til første
+        }
+    } else if (endX - startX > minSwipeDistance) {
+        //swipe til højre - forrige
+        manualSlideIndex--;
+        if (manualSlideIndex < 1) {
+            manualSlideIndex = document.getElementsByClassName ('slide-box').length;
+        }
+    }
+    showSlidesManual (manualSlideIndex);
+}
+
 
 function currentSlide (n) {
     manualSlideIndex = n;
@@ -14,8 +44,10 @@ function showSlidesManual (n) {
     let i;
     let slides = document.getElementsByClassName ("slide-box");
     let dots =document.getElementsByClassName ("dot");
-    if (n > slides.length) {manualSlideIndex = 1}
-    if (n < 1) {manualSlideIndex = slides.length}
+   
+    if (n > slides.length) {manualSlideIndex = 1;}
+    if (n < 1) {manualSlideIndex = slides.length;}
+    
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -24,8 +56,8 @@ function showSlidesManual (n) {
         dots[i].className = dots[i].className.replace(" active","");
     }
 
-    slides[manualSlideIndex-1].style.display = "block";
-    dots[manualSlideIndex-1].className += " active";
+    slides[manualSlideIndex - 1].style.display = "block";
+    dots[manualSlideIndex - 1].className += " active";
 }
 
 //Automatisk slideshow
